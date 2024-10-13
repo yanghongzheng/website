a2955c4688552b9246cdcfa3ae889efe97eae35c
rongfengliang <1141591465@qq.com>
2024-10-13 13:53:23 +0800

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'yanghongzheng', // Usually your GitHub org/user name.
   projectName: 'mywebsite', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
